71c6b88c5c9eacf47cc0e5b9bc0254e669e8dc20
Suzy Hwang <soyeongsuzyhwang@gmail.com>
2021-10-09 02:04:35 +0900

    [LoginPage][Login] 로그인 페이지 서버 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  useHistory,
+  Redirect,
+} from "react-router-dom";
+import axios from "axios";
 import Modal from "./components/Modal/Modal";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MainPage from "./pages/MainPage";
@@ -6,23 +14,35 @@ import MyPage from "./pages/MyPage";
 import SignupPage from "./pages/SignupPage";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userinfo, setUserinfo] = useState(null);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    axios.post("https://localhost:4000/signout").then((res) => {
+      setUserinfo(null);
+      setIsLogin(false);
+      history.push("/");
+    });
+  };
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <MainPage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-        <Route path="/signup">
-          <SignupPage />
-        </Route>
-        <Route path="/mypage">
-          <MyPage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Switch>
+    //     <Route exact path="/">
+    //       <MainPage />
+    //     </Route>
+    //     <Route path="/login">
+    //       <LoginPage />
+    //     </Route>
+    //     <Route path="/signup">
+    //       <SignupPage />
+    //     </Route>
+    //     <Route path="/mypage">
+    //       <MyPage />
+    //     </Route>
+    //   </Switch>
+    // </BrowserRouter>
+    <LoginPage isLogin={isLogin} />
   );
 }
 
