06af4aa964243272a94c40e702df0d169fa51a21
Jung InYong <iysh321@gmail.com>
2021-10-10 22:37:17 +0900

    [Endpoint][Photo] photo 엔드포인트 구현

--- server/controller/photo/index.js
+++ server/controller/photo/index.js
@@ -3,4 +3,7 @@ module.exports = {
   post: require("./post"),
   put: require("./put"),
   delete: require("./delete"),
+  info_get: require("./info_get"),
+  info_post: require("./info_post"),
+  info_put: require("./info_put"),
 };
