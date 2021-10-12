040ba10e4ae65be96ee074300074c1d8b7ab5aa6
Jung InYong <iysh321@gmail.com>
2021-10-13 01:21:36 +0900

    [Endpoint][Photo] weather 부분구현

--- server/controller/photo/info_post.js
+++ server/controller/photo/info_post.js
@@ -7,24 +7,24 @@ module.exports = async (req, res) => {
 
   // 각 필요한 데이터가 없을때 결과값
   if (!id) {
-    res.status(409).json({ message: "사진 id 정보가 없습니다." });
+    return res.status(409).json({ message: "사진 id 정보가 없습니다." });
   } else if (!weather) {
-    res.status(409).json({ message: "날씨 정보가 없습니다." });
+    return res.status(409).json({ message: "날씨 정보가 없습니다." });
   } else if (!date) {
-    res.status(409).json({ message: "날짜 정보가 없습니다." });
+    return res.status(409).json({ message: "날짜 정보가 없습니다." });
   } else if (!area) {
-    res.status(409).json({ message: "지역 정보가 없습니다." });
+    return res.status(409).json({ message: "지역 정보가 없습니다." });
   } else if (!comment) {
-    res.status(409).json({ message: "comment가 없습니다." });
+    return res.status(409).json({ message: "comment가 없습니다." });
   } else if (!filename) {
-    res.status(409).json({ message: "filname이 없습니다." });
+    return res.status(409).json({ message: "filname이 없습니다." });
   }
 
   // 사진이 저장된 칼럼부분을 찾아 사진정보를 저장
   const info = await photo.update({ userId: userId, weather: weather, date: date, area: area, comment: comment }, { where: { id: id, filename: filename } });
   console.log("---------------------", info);
   if (info) {
-    res.status(404).json({ message: "저장된 사진을 찾을 수 없습니다." });
+    return res.status(404).json({ message: "저장된 사진을 찾을 수 없습니다." });
   }
   return res.status(200).json({ message: "사진정보가 저장완료 되었습니다." });
 };
