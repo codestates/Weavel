a5694b07fee67994efde6d54792c02919bf7dde9
Jung InYong <83904581+iysh321@users.noreply.github.com>
2021-10-17 20:08:39 +0900

    Update weatherAPI.js

--- server/router/weatherAPI.js
+++ server/router/weatherAPI.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const { weatherAPIController } = require("../controller");
-const { cityarea } = require("../middleware/cityArea");
+const { cityarea } = require("../middleware/cityarea");
 
 router.get("/", cityarea, weatherAPIController.weather_data);
 
