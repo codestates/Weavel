dba34b8e88f9d03df8d7d4eb7538b2e13409569d
Jung InYong <iysh321@gmail.com>
2021-11-19 22:39:25 +0900

    [배포][자동화] 파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,4 +7,6 @@ export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export API_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+npx sequelize-cli db:migrate
+npx sequelize-cli db:seed:all
 authbind --deep pm2 start index.js
