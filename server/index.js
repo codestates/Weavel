dac3da9fc58b351c3a165280f32a0323f5b3ed95
Jung InYong <iysh321@gmail.com>
2021-11-22 22:48:46 +0900

    weather API 시간값 변경

--- server/index.js
+++ server/index.js
@@ -20,55 +20,55 @@ app.use(
 );
 
 // 전국 날씨 API Get 요청 예약 (데이터 받는 가능시간 00시 03시, 06시, 09시, 12시, 15시, 18시, 21시)
-schedule.scheduleJob("0 0 22 * * *", () => {
+schedule.scheduleJob("0 0 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=01");
 });
-schedule.scheduleJob("0 3 22 * * *", () => {
+schedule.scheduleJob("0 5 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=02");
 });
-schedule.scheduleJob("0 6 22 * * *", () => {
+schedule.scheduleJob("0 10 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=03");
 });
-schedule.scheduleJob("0 9 22 * * *", () => {
+schedule.scheduleJob("0 15 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=04");
 });
-schedule.scheduleJob("0 11 22 * * *", () => {
+schedule.scheduleJob("0 20 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=05");
 });
-schedule.scheduleJob("0 12 22 * * *", () => {
+schedule.scheduleJob("0 25 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=06");
 });
-schedule.scheduleJob("0 14 22 * * *", () => {
+schedule.scheduleJob("0 30 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=07");
 });
-schedule.scheduleJob("0 16 22 * * *", () => {
+schedule.scheduleJob("0 35 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=08");
 });
-schedule.scheduleJob("0 19 22 * * *", () => {
+schedule.scheduleJob("0 40 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=09");
 });
-schedule.scheduleJob("0 20 22 * * *", () => {
+schedule.scheduleJob("0 45 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=10");
 });
-schedule.scheduleJob("0 30 22 * * *", () => {
+schedule.scheduleJob("0 55 6 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=11");
 });
-schedule.scheduleJob("0 35 22 * * *", () => {
+schedule.scheduleJob("0 0 7 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=12");
 });
-schedule.scheduleJob("0 38 22 * * *", () => {
+schedule.scheduleJob("0 5 7 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=13");
 });
-schedule.scheduleJob("0 41 22 * * *", () => {
+schedule.scheduleJob("0 10 7 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=14");
 });
-schedule.scheduleJob("0 44 22 * * *", () => {
+schedule.scheduleJob("0 15 7 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=15");
 });
-schedule.scheduleJob("0 50 22 * * *", () => {
+schedule.scheduleJob("0 20 7 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=16");
 });
-schedule.scheduleJob("0 55 22 * * *", () => {
+schedule.scheduleJob("0 25 7 * * *", () => {
   axios.get("https://server.weavel.site/weatherAPI?id=17");
 });
 
