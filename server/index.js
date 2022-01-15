5aa1bf6db2fd0e6b40df8f7c1db79085fb012b4f
Jung InYong <iysh321@gmail.com>
2022-01-15 23:56:47 +0900

    [index] 분리작업

--- server/index.js
+++ server/index.js
@@ -5,92 +5,12 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const axios = require("axios");
 const express = require("express");
-const app = express();
+const app = express("./app");
 const schedule = require("node-schedule");
-
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-  }),
-);
-
-// 전국 날씨 API Get 요청 예약 (데이터 받는 가능시간 00시 03시, 06시, 09시, 12시, 15시, 18시, 21시)
-schedule.scheduleJob("0 0 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=01");
-});
-schedule.scheduleJob("0 5 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=02");
-});
-schedule.scheduleJob("0 10 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=03");
-});
-schedule.scheduleJob("0 15 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=04");
-});
-schedule.scheduleJob("0 20 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=05");
-});
-schedule.scheduleJob("0 25 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=016");
-});
-schedule.scheduleJob("0 30 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=07");
-});
-schedule.scheduleJob("0 35 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=08");
-});
-schedule.scheduleJob("0 40 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=09");
-});
-schedule.scheduleJob("0 45 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=10");
-});
-schedule.scheduleJob("0 55 6 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=11");
-});
-schedule.scheduleJob("0 0 7 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=12");
-});
-schedule.scheduleJob("0 5 7 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=13");
-});
-schedule.scheduleJob("0 10 7 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=14");
-});
-schedule.scheduleJob("0 15 7 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=15");
-});
-schedule.scheduleJob("0 20 7 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=16");
-});
-schedule.scheduleJob("0 25 7 * * *", () => {
-  axios.get("https://server.weavel.site/weatherAPI?id=17");
-});
-
-//라우터 경로
-const userrouter = require("./router/user");
-const photorouter = require("./router/photo");
-const weatherrouter = require("./router/weather");
-const weatherAPIrouter = require("./router/weatherAPI");
-
-//특정 API를 받았을 때
-app.use("/user", userrouter);
-app.use("/photo", photorouter);
-app.use("/weather", weatherrouter);
-app.use("/weatherAPI", weatherAPIrouter);
 
 //서버실행
 const PORT = process.env.SERVER_PORT || 80;
 const HOST = process.env.SERVER_HOST;
-
-app.get("/", (req, res) => {
-  res.status(201).send("Hello World");
-});
 
 app.listen(PORT, () => {
   console.log(`Server Listening on ${PORT}`);
