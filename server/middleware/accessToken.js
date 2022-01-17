9c8be6c3f2190e73661eb06157a79df1022205b1
Jung InYong <iysh321@gmail.com>
2022-01-17 23:12:06 +0900

    [middleware] 변수 수정

--- server/middleware/accessToken.js
+++ server/middleware/accessToken.js
@@ -23,24 +23,28 @@ module.exports = {
       }
     }
 
-    function tokenConfim(authHeader) {
-      const token = authHeader.split(" ")[1];
-      jwt.verify(token, process.env.ACCESS_SECRET, async (err, decode) => {
-        if (err) {
-          new errorMessage("유효하지").respond();
-        }
-        const Info = await user.findOne({
-          where: { id: decode.id },
-        });
-        if (!Info) {
-          new errorMessage("일치하는 유저가 존재하지").respond();
-        }
-        req.userId = Info.id;
-        next();
-      });
+    function accessTokenConfim(authHeader) {
+      const AccessToken = authHeader.split(" ")[1];
+      jwt.verify(
+        AccessToken,
+        process.env.ACCESS_SECRET,
+        async (err, decode) => {
+          if (err) {
+            new errorMessage("유효하지").respond();
+          }
+          const findUser = await user.findOne({
+            where: { id: decode.id },
+          });
+          if (!findUser) {
+            new errorMessage("일치하는 유저가 존재하지").respond();
+          }
+          req.userId = Info.id;
+          next();
+        },
+      );
     }
 
     checkAccessToken(authHeader);
-    tokenConfim(authHeader);
+    accessTokenConfim(authHeader);
   },
 };
