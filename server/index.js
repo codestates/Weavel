04ae9335435107c793886885dc01178ffd2157ce
Jung InYong <iysh321@gmail.com>
2022-01-20 16:55:16 +0900

    [weatherData] 시간, URL 수정

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const app = require("./app.js");
-const weatherDataTimer = require("./weatherDataTimer.js");
 
 const PORT = process.env.SERVER_PORT || 80;
 const HOST = process.env.SERVER_HOST;
-
-weatherDataTimer.scheduleJob;
 
 app.listen(PORT, () => {
   console.log(`Server Listening on ${PORT}`);
