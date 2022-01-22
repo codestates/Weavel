d5924193bdcc79f079f64ca40f21073fbee2dc5f
Jung InYong <iysh321@gmail.com>
2022-01-22 14:22:59 +0900

    [controller][weatherDataAPI] 누락데이터 해결 코드 작성

--- server/controller/weatherAPI/weather_data.js
+++ server/controller/weatherAPI/weather_data.js
@@ -54,65 +54,84 @@ module.exports = async (req, res) => {
       }
     }
 
-    function downloadWeatherDataAPI(areaArray) {
+    function downloadWeatherDataAPI(area) {
       const url =
         "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
       const key = process.env.API_KEY;
+      const day = moment().format("YYYYMMDD");
+      const base_time = "0500"; // 호출 데이터 05시 고정
+      // String(Number(moment().format("HH"))) - 1 + "00"; // 현재시간 -1
+      const nx = area[0];
+      const ny = area[1];
+      const nzCode = area[2];
+      const dataType = "XML";
+      const allUrl =
+        url +
+        "?serviceKey=" +
+        key +
+        "&pageNo=1&numOfRows=10000" +
+        "&dataType=" +
+        dataType +
+        "&base_date=" +
+        day +
+        "&base_time=" +
+        base_time +
+        "&nx=" +
+        nx +
+        "&ny=" +
+        ny;
+      console.log(allUrl);
 
-      areaArray.map((el) => {
-        const day = moment().format("YYYYMMDD");
-        const base_time = "0500"; // 호출 데이터 05시 고정
-        // String(Number(moment().format("HH"))) - 1 + "00"; // 현재시간 -1
-        const nx = el[0];
-        const ny = el[1];
-        const nzCode = el[2];
-        const dataType = "XML";
+      request(allUrl, (err, res, body) => {
+        $ = cheerio.load(body);
+        $("item").each(function (idx) {
+          const nxFindXML = $(this).find("nx").text();
+          const nyFindXML = $(this).find("ny").text();
+          const dateFindXML = $(this).find("fcstDate").text();
+          const timeFindXML = $(this).find("fcstTime").text();
+          const categoryFindXML = $(this).find("category").text();
+          const valueFindXML = $(this).find("fcstValue").text();
 
-        // url
-        const allUrl =
-          url +
-          "?serviceKey=" +
-          key +
-          "&pageNo=1&numOfRows=10000" +
-          "&dataType=" +
-          dataType +
-          "&base_date=" +
-          day +
-          "&base_time=" +
-          base_time +
-          "&nx=" +
-          nx +
-          "&ny=" +
-          ny;
-        console.log(allUrl);
-
-        request(allUrl, (err, res, body) => {
-          $ = cheerio.load(body);
-          $("item").each(function (idx) {
-            const nxFindXML = $(this).find("nx").text();
-            const nyFindXML = $(this).find("ny").text();
-            const dateFindXML = $(this).find("fcstDate").text();
-            const timeFindXML = $(this).find("fcstTime").text();
-            const categoryFindXML = $(this).find("category").text();
-            const valueFindXML = $(this).find("fcstValue").text();
-
-            saveWeather_data(
-              nxFindXML,
-              nyFindXML,
-              nzCode,
-              dateFindXML,
-              timeFindXML,
-              categoryFindXML,
-              valueFindXML,
-            );
-          });
+          saveWeather_data(
+            nxFindXML,
+            nyFindXML,
+            nzCode,
+            dateFindXML,
+            timeFindXML,
+            categoryFindXML,
+            valueFindXML,
+          );
         });
-        sleepTime(5000);
       });
+      sleepTime(5000);
+    }
+
+    function findMissingData(areaArray) {
+      const result = areaArray.filter((area) => {
+        const nx = area[0];
+        const ny = area[1];
+        let findWeatherData = weather_data.findOne({
+          where: { nx: nx, ny: ny },
+        });
+        if (!findWeatherData) return area;
+      });
+      return result;
+    }
+
+    function saveWeatherData(areaArray) {
+      areaArray.map((area) => {
+        downloadWeatherDataAPI(area);
+      });
+
+      const missingData = findMissingData(areaArray);
+      if (missingData.length === 0) return;
+
+      saveWeatherData(missingData);
     }
 
     deleteOldWeatherData(cityId);
-    downloadWeatherDataAPI(areaArray);
+    saveWeatherData(areaArray);
+
     return res
       .status(201)
       .json({ message: "해당 city의 모든 날씨를 받아왔습니다" });
