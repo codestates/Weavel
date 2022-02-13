1c938cca4fdbcc14c5da30b4aaccee2e29c30a67
Jung InYong <iysh321@gmail.com>
2022-02-13 16:07:54 +0900

    [middleware][accessToken] accessToken TDD

--- server/middleware/tests/acessToken.test.js
+++ server/middleware/tests/acessToken.test.js
@@ -1,19 +1,18 @@
-const httpsMocks = require("node-mocks-http");
-const { accessToken } = require("../accessToken.js");
+const httpMocks = require("node-mocks-http");
+const { accessToken, finduser } = require("../accessToken.js");
 const faker = require("faker");
 const jwt = require("jsonwebtoken");
-const { user } = require("../../models");
 
 jest.mock("jsonwebtoken");
-// jest.mock("../../models/user.js");
+jest.mock("../../models");
 
 describe("Token Middleware", () => {
   it("토큰이 없을 경우 statusCode 401을 리턴한다.", async () => {
-    const request = httpsMocks.createRequest({
+    const request = httpMocks.createRequest({
       method: "GET",
       url: "/user",
     });
-    const response = httpsMocks.createResponse();
+    const response = httpMocks.createResponse();
     const next = jest.fn();
 
     await accessToken(request, response, next);
@@ -26,12 +25,12 @@ describe("Token Middleware", () => {
   });
 
   it("토큰을 찾을 수 없는경우 statusCode 401을 리턴한다.", async () => {
-    const request = httpsMocks.createRequest({
+    const request = httpMocks.createRequest({
       method: "GET",
       url: "/user",
       headers: { authorization: "basic" },
     });
-    const response = httpsMocks.createResponse();
+    const response = httpMocks.createResponse();
     const next = jest.fn();
 
     await accessToken(request, response, next);
@@ -45,12 +44,12 @@ describe("Token Middleware", () => {
 
   it("토큰이 유효하지 않을 경우 401을 리턴한다.", async () => {
     const token = faker.random.alphaNumeric(128);
-    const request = httpsMocks.createRequest({
+    const request = httpMocks.createRequest({
       method: "GET",
       url: "/user",
       headers: { Authorization: `Bearer ${token}` },
     });
-    const response = httpsMocks.createResponse();
+    const response = httpMocks.createResponse();
     const next = jest.fn();
     jwt.verify = jest.fn((token, secret, callback) => {
       callback(new Error("bad token"), undefined);
@@ -61,6 +60,31 @@ describe("Token Middleware", () => {
     expect(response.statusCode).toBe(401);
     expect(response._getJSONData().message).toBe(
       "access token 유효하지 않습니다.",
+    );
+    expect(next).not.toBeCalled();
+  });
+
+  it("returns 401 when cannot find a user by id from the JWT", async () => {
+    const token = faker.random.alphaNumeric(128);
+    const userId = faker.random.alphaNumeric(32);
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/user",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const response = httpMocks.createResponse();
+    const next = jest.fn();
+
+    jwt.verify = jest.fn((token, secret, callback) => {
+      callback(undefined, { id: userId });
+    });
+    finduser = jest.fn((id) => Promise.resolve(undefined));
+
+    await accessToken(request, response, next);
+
+    expect(response.statusCode).toBe(401);
+    expect(response._getJSONData().message).toBe(
+      "access token 일치하는 유저가 존재하지 않습니다.",
     );
     expect(next).not.toBeCalled();
   });
