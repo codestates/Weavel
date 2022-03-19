311d8577eb7c139fc451d6edb12c94c7ceaa16ba
Suzy Hwang <soyeongsuzyhwang@gmail.com>
2022-03-19 16:00:16 +0900

    [MyPage][PhotoInfo] 사진 수정 구현 사진 정보 수정 구현중

---
+++ client/src/components/PhotoEditModalNew/PhotoEditModalNew.js
@@ -0,0 +1,279 @@
+import react, { useState, useEffect } from "react";
+import { areaData } from "../../components/PhotoUploadModal/SearchData";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  PhotoUploadContainer,
+  PhotoUploadFileContainer,
+  InputContainer,
+  PhotoUploadInput,
+  WeatherButton,
+  BottomButtonContainer,
+  UploadButton,
+  CancelButton,
+} from "../PhotoUploadModalNew/PhotoUploadModalNew.style";
+
+function PhotoEditModalNew({ photoEditHandler, isSelectInfo }) {
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.authReducer.accessToken);
+  const weatherArr = ["맑음", "구름", "비", "눈"];
+  const [isClick, setIsClick] = useState({
+    0: false,
+    1: false,
+    2: false,
+    3: false,
+  });
+  const [file, setFile] = useState([]);
+  const [date, setDate] = useState("");
+  const [area, setArea] = useState("");
+  const [photoId, setPhotoId] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
+  const [isFilled, setIsFilled] = useState(true);
+  const [dataResult, setDataResult] = useState([]);
+  useEffect(() => {
+    const selectedInfo = isSelectInfo[0];
+    if (selectedInfo) {
+      console.log(selectedInfo, "@@");
+      let comment = selectedInfo.comment;
+      let area = selectedInfo.area;
+      let weather = selectedInfo.weather;
+      let date = selectedInfo.date;
+      let file = {
+        preview: `${process.env.REACT_APP_API_URL}/${selectedInfo.image}`,
+      };
+      let photoId = selectedInfo.id;
+      setComment(comment);
+      setArea(area);
+      setDate(date);
+      const defaultValue = {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+      };
+      defaultValue[weather] = true;
+      setIsClick(defaultValue);
+      setWeather(weather);
+      setFile(file);
+      setPhotoId(photoId);
+    }
+  }, []);
+
+  const inputHandler = (e) => {
+    if (e.target.name === "file" && e.target.files[0]) {
+      e.target.files[0].preview = URL.createObjectURL(e.target.files[0]);
+      setFile(e.target.files[0]);
+    } else if (e.target.name === "date") {
+      setDate(e.target.value);
+    } else if (e.target.name === "area") {
+      setArea(e.target.value);
+    } else if (e.target.name === "comment") {
+      setComment(e.target.value);
+    } else if (typeof Number(e.target.name) === "number") {
+      const defaultValue = {
+        0: false,
+        1: false,
+        2: false,
+        3: false,
+      };
+      defaultValue[e.target.name] = true;
+      setIsClick(defaultValue);
+      setWeather(e.target.name);
+    }
+  };
+
+  useEffect(() => {
+    if (file.preview && date && area && comment && weather) {
+      setIsFilled(false);
+    } else {
+      setIsFilled(true);
+    }
+  }, [file, date, area, comment, weather]);
+
+  useEffect(() => {
+    if (area.length > 0) {
+      setDataResult(
+        areaData.filter((data) => {
+          if (data === area) {
+            return false;
+          }
+          return data.includes(area);
+        })
+      );
+    }
+  }, [area]);
+
+  const searchDataHandler = (e) => {
+    setArea(e.target.innerText);
+  };
+
+  const formData = new FormData();
+  formData.append("image", file);
+
+  // 사진 수정
+  const handlePhotoEdit = (e) => {
+    console.log(formData.values(), "@@@");
+    if (formData) {
+      for (let pair of formData.entries()) {
+        console.log(pair);
+        console.log(typeof pair[1]);
+        //string
+        //object
+        if (typeof pair[1] === "string") {
+          handlePhotoInfoEdit();
+        } else if (typeof pair[1] === "object") {
+          axios
+            .put(
+              `${process.env.REACT_APP_API_URL}/photo?id=${photoId}`,
+              formData,
+              {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                  "Content-Type": "multipart/form-data",
+                },
+              },
+              { withCredentials: true }
+            )
+            .then((res) => {
+              console.log(res);
+              handlePhotoInfoEdit(e, res.data.data);
+            })
+            .catch((err) => {
+              console.error(`signin error: ${err.message}`);
+            });
+        }
+      }
+    }
+  };
+
+  // 사진 정보 수정
+  const handlePhotoInfoEdit = (e, photo) => {
+    console.log(photo, "@DAFDF");
+    console.log(photoId);
+    console.log(date);
+    console.log(area);
+    axios
+      .patch(
+        `${process.env.REACT_APP_API_URL}/photo/info`,
+        {
+          id: photoId,
+          filename: file.preview.slice(20),
+          date,
+          area,
+          comment,
+          weather,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        // setAllPhotoInfo([
+        //   {
+        //     id: photo.id,
+        //     image: photo.photoPath,
+        //     filename: photo.filename,
+        //     date,
+        //     area,
+        //     comment,
+        //     weather,
+        //   },
+        //   ...allPhotoInfo,
+        // ]);
+        console.log(res, "####");
+      })
+      .catch((err) => {
+        console.error(`signin error: ${err.message}`);
+      });
+  };
+  return (
+    <PhotoUploadContainer onClick={(e) => e.stopPropagation()}>
+      <PhotoUploadFileContainer>
+        <div id="img_container">
+          {file.preview ? (
+            <img id="preview_img" src={file.preview} alt="preview" />
+          ) : (
+            <img src={"./images/upload.svg"} alt="upload" />
+          )}
+        </div>
+        <input type="file" onChange={(e) => inputHandler(e)} name="file" />
+      </PhotoUploadFileContainer>
+      <label>날짜</label>
+      <InputContainer>
+        <PhotoUploadInput
+          type="date"
+          name="date"
+          onChange={(e) => inputHandler(e)}
+          value={date}
+        />
+      </InputContainer>
+      <label>지역</label>
+      <InputContainer>
+        <PhotoUploadInput
+          placeholder="지역을 검색해 보세요"
+          name="area"
+          onChange={(e) => inputHandler(e)}
+          value={area}
+        />
+
+        {dataResult[0] ? (
+          <ul>
+            {dataResult.map((data, idx) => {
+              if (idx < 5) {
+                return (
+                  <li onClick={(e) => searchDataHandler(e)} key={idx}>
+                    {data}
+                  </li>
+                );
+              }
+            })}
+          </ul>
+        ) : (
+          ""
+        )}
+      </InputContainer>
+      <label>날씨</label>
+      <InputContainer row={"row"}>
+        {weatherArr.map((weather, idx) => {
+          return (
+            <WeatherButton
+              key={idx}
+              name={idx}
+              onClick={(e) => inputHandler(e)}
+              isClick={isClick[idx]}
+            >
+              {weather}
+            </WeatherButton>
+          );
+        })}
+      </InputContainer>
+      <label>코멘트</label>
+      <InputContainer>
+        <PhotoUploadInput
+          placeholder="25글자 이내로 남기고 싶은 코멘트를 적어주세요"
+          maxLength="25"
+          name="comment"
+          value={comment}
+          onChange={(e) => inputHandler(e)}
+        />
+      </InputContainer>
+      <BottomButtonContainer>
+        <UploadButton
+          disabled={isFilled}
+          isFilled={isFilled}
+          onClick={handlePhotoEdit}
+        >
+          수정
+        </UploadButton>
+        <CancelButton onClick={photoEditHandler}>취소</CancelButton>
+      </BottomButtonContainer>
+    </PhotoUploadContainer>
+  );
+}
+
+export default PhotoEditModalNew;
---
+++ client/src/components/PhotoEditModalNew/PhotoEditModalNew.style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const PhotoEditContainer = styled.div`
+  max-width: 396px;
+  width: 100%;
+  height: 640px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #ffff;
+  padding: 40px 108px;
+  border-radius: 4px;
+  z-index: 99;
+  label {
+    width: 100%;
+    font-size: 16px;
+    color: #444444;
+    text-align: left;
+    margin: 14px 0 6px 0;
+  }
+`;
